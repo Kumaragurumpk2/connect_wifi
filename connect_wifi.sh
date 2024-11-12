9316c5a0779c500b742631be08181cb27712d200
Kumaragurumpk2 <kumaragurubackup06@gmail.com>
2024-11-12 13:03:07 +0530

    Update connect_wifi.sh

--- connect_wifi.sh
+++ connect_wifi.sh
@@ -5,7 +5,7 @@ SSID="Guru"
 PASSWORD="1234567891"
 SECURITY="WPA2"
 
-INTERFACE="wlan0" # Replace with the correct interface if it's different
+INTERFACE="wlp2s0" # Replace with the correct interface if it's different
 
 # Disconnect any current connection
 sudo nmcli device disconnect "$INTERFACE"
